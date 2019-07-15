71140613b94cfece6ac9146cd77d576657710855
Andrew Hager <scott.hager121@gmail.com>
2019-07-15 13:34:51 -0400

    intitial layout

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
 import React from 'react';
+// import profile from './assetts/user.png';
+// import hamburger from './assetts/menu.png';
 
 function App() {
   return (
     <div className="App">
       <header className="boardTitle">
-        <img src="#" className="boardTitle__hamburger" />
-        <h3 class="boardTitle__h3">Board Title</h3>
+        <h3 className="boardTitle__h3">Sprint Title</h3>
+        {/* <img src={profile} alt="profileBtn" id="profileBtn"></img> */}
       </header>
-      <img src="./assetts/user.png" id="userPhoto" />
-      <img src="./assetts/user.png" id="userPhoto" />
-      <img src="./assetts/user.png" id="userPhoto" />
 
-      <section class="inProgress">
-        <p>In-Progress</p>
-        <div class="inProgress__cardBg">
-          <div class="inProgress__card">
-            <h3>Task Title</h3>
-
+      <section class="boards__section">
+        <section className="board">
+          <div className="board__wrap">
+            <h3>Back Log</h3>
+            <div className="board__section">
+              <div className="board__card boardborder--blue">
+                <h3>Task Title</h3>
+                <p>
+                  Task details and guidelines lorem ipsum lorem ipsum loreum ipsum.
+             </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section class="scheduled">
+        <section className="board">
+          <div className="board__wrap">
+            <h3>Assigned</h3>
+            <div className="board__section">
+              <div className="board__card boardborder--lightblue">
+                <h3>Task Title</h3>
+                <p>
+                  Task details and guidelines lorem ipsum lorem ipsum loreum ipsum.
+             </p>
+              </div>
+            </div>
+          </div>
+        </section>
 
+        <section className="board">
+          <div className="board__wrap">
+            <h3>In-Progress</h3>
+            <div className="board__section">
+              <div className="board__card boardborder--teal">
+                <h3>Task Title</h3>
+                <p>
+                  Task details and guidelines lorem ipsum lorem ipsum loreum ipsum.
+             </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="board">
+          <div class="board__wrap">
+            <h3>Completed</h3>
+            <div className="board__section">
+              <div className="board__card boardborder--lightgreen">
+                <h3>Task Title</h3>
+                <p>
+                  Task details and guidelines lorem ipsum lorem ipsum loreum ipsum.
+             </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
-      <section class="newTask">
-      </section>
-    </div>
+    </div >
   );
 }
 
