e3195a318e208911bd2205e172e34dcdf2a7f7d3
Andrew Hager <scott.hager121@gmail.com>
2019-07-18 09:44:19 -0400

    some more react

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
-export default function Card() {
-    return (
-        <div className="board__card">
-            <h3>Task Title</h3>
-            <h4>Due: 01/01/01</h4>
-        </div>
-    )
+export default class Card extends Component {
+
+    getCardColor() {
+        if (this.props.task.team === 'dev') {
+            return 'devColor'
+        }
+    }
+    render() {
+        const task = this.props.task;
+        return (
+            <div className={"board__card " + this.getCardColor()}>
+                <h3>{task.title}</h3>
+                <h4>Due: 01/01/01</h4>
+            </div>
+        )
+    }
 }
--- src/Stack.js
+++ src/Stack.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Card from './Card'
 import './index.css'
 
-
-export default function Stack() {
-    return (
-        <section className="board">
-            <div className="board__wrap">
-                <h3>Assigned</h3>
-                <div className="board__section">
-                    <Card />
+export default class Stack extends Component {
+    constructor() {
+        super()
+        this.state = {
+            tasks: [
+                {
+                    title: "Task Title"
+                },
+                {
+                    title: "Task Title2",
+                    team: "dev"
+                },
+                {
+                    title: "Task Title3"
+                }
+            ]
+        }
+    }
+    render() {
+        var tasksList = this.state.tasks.map(function (task) {
+            return <Card task={task} />;
+        })
+        return (
+            <section className="board" >
+                <div className="board__wrap">
+                    <h3>Assigned</h3>
+                    <div className="board__section">
+                        {tasksList}
+                    </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
