eec22cafa0220c7511019deed60acd096a35431e
Andrew Hager <scott.hager121@gmail.com>
2019-07-19 13:40:53 -0400

    arrays finished

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Stack from './Stack';
 import '../index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="boardTitle">
-        <h3 className="boardTitle__h3">Sprint Title</h3>
-      </header>
-      <div className="boards__section ">
-        <Stack />
-        <Stack />
-        <Stack />
-        <Stack />
-      </div>
-    </div >
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      stacks: {
+        backlog: [
+          {
+            title: "Task Title",
+            category: "code"
+          },
+          {
+            title: "Task Title",
+            category: "testing"
+          },
+          {
+            title: "Task Title",
+            category: "code"
+          }
+        ],
+        assigned: [
+          {
+            title: "Task Title",
+            category: "code"
+          },
+          {
+            title: "Task Title",
+            category: "code"
+          },
+          {
+            title: "Task Title",
+            category: "testing"
+          }
 
-export default App;
+        ],
+        inprogress: [
+          {
+            title: "Task Title",
+            category: "code"
+          },
+          {
+            title: "Task Title",
+            category: "resources"
+          },
+          {
+            title: "Task Title",
+            category: "testing"
+          }
+
+        ],
+        complete: [
+          {
+            title: "Task Title",
+            category: "resources"
+          },
+          {
+            title: "Hello There",
+            category: "research"
+          },
+          {
+            title: "Task Title",
+            category: "design"
+          }
+
+        ]
+      }
+    }
+  }
+  render() {
+    return (
+      <div className="App" >
+        <header className="boardTitle">
+          <h3 className="boardTitle__h3">Sprint Title</h3>
+        </header>
+        <div className="boards__section ">
+          <Stack tasks={this.state.stacks.backlog} />
+          <Stack tasks={this.state.stacks.assigned} />
+          <Stack tasks={this.state.stacks.inprogress} />
+          <Stack tasks={this.state.stacks.complete} />
+        </div>
+      </div >
+    );
+  }
+}
--- src/Components/Stack.js
+++ src/Components/Stack.js
@@ -3,75 +3,10 @@ import Card from './Card.js'
 import '../index.css'
 
 export default class Stack extends Component {
-    constructor() {
-        super()
-        this.state = {
-            stacks: {
-                backlog: [
-                    {
-                        title: "Task Title",
-                        category: "code"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "testing"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "code"
-                    }
-                ],
-                assigned: [
-                    {
-                        title: "Task Title",
-                        category: "code"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "code"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "testing"
-                    }
 
-                ],
-                inprogress: [
-                    {
-                        title: "Task Title",
-                        category: "code"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "resources"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "testing"
-                    }
-
-                ],
-                complete: [
-                    {
-                        title: "Task Title",
-                        category: "resources"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "research"
-                    },
-                    {
-                        title: "Task Title",
-                        category: "design"
-                    }
-
-                ]
-            }
-        }
-    }
     // pass stacks as a prop
     render() {
-        var tasksList = this.state.stacks.complete.map((task, index) => {
+        var tasksList = this.props.tasks.map((task, index) => {
             return <Card task={task} key={index} />
         })
         return (
