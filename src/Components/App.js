3c34cdab6d3474697897b07e0163ad6dc79982bc
Blake Walker <blakewdesigns@gmail.com>
2019-07-22 13:38:20 -0400

    Installed SASS
    SASS installed into React. Updated React version to be compatible. Changed CSS file to SCSS. Changed all component CSS imports to `import "./styles.scss`.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Stack from './Stack';
-import '../index.css';
+import '../index.scss';
 
 export default class App extends Component {
   constructor() {
--- src/Components/Stack.js
+++ src/Components/Stack.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card.js'
-import '../index.css'
+import '../index.scss'
 
 export default class Stack extends Component {
 
