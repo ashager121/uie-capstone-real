5fbf1ec637ae1e9ea33d048dbdbf26dc364d96f7
Andrew Hager <scott.hager121@gmail.com>
2019-07-24 14:33:20 -0400

    Modal Working Yo

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Stack from './Stack';
 import './../sass/App.scss';
 import { ModalRoute } from 'react-router-modal';
+import Modal from './Modal';
+import {Link}from 'react-router-dom';
+import 'react-router-modal/css/react-router-modal.css';
 
 
 
@@ -9,18 +12,22 @@ import { ModalRoute } from 'react-router-modal';
 export default class App extends Component {
   constructor() {
     super()
-    this.state = {
+    this.state = //mockdata
+     {
       stacks: {
         backlog: [
           {
+            _id: 1,
             title: "Task Title",
             category: "code"
           },
           {
+            _id: 2,
             title: "Task Title",
             category: "testing"
           },
           {
+            _id: 3,
             title: "Task Title",
             category: "code"
           }
@@ -102,12 +109,11 @@ export default class App extends Component {
           <Stack tasks={this.state.stacks.complete} />
         </div>
         <ModalRoute
-          component={this.Details}
+          component={Modal}
           path='/dashboard/details/:taskId'
           parentPath='/dashboard/'
         />
-        {/* <Link to=‘/dashboard/details/66’>show Details66</Link> */}
-      </div >
+      </div>
     );
   }
-}
+}                                                                     
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 // import Comment from './assets/comment.png';
 // import Priority from './assets/priority.png';
 import './../sass/App.scss';
+// import Modal from 'react-router-modal';
+// import Photo from '../assets/user.png';
+// import PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom';
 
-export default class Card extends Component {
+class Card extends Component {
 
     getCardColor() {
         if (this.props.task.category === 'code') {
@@ -18,10 +22,13 @@ export default class Card extends Component {
             return 'board__card--resources'
         }
     }
+    opencard= () =>{
+        this.props.history.push('/dashboard/details/'+ this.props.task._id)
+    }
     render() {
         const task = this.props.task;
         return (
-            <div className={"board__card " + this.getCardColor()}>
+            <div onClick={this.opencard} className={"board__card " + this.getCardColor()}>
                 <h3>{task.title}</h3>
                 <h4>Due: 01/01/01</h4>
                 {/* <img src={Comment} alt="Comment" /> */}
@@ -30,70 +37,4 @@ export default class Card extends Component {
         )
     }
 }
-
-// // Modal Starts Here?
-// function MyVerticallyCenteredModal(props) {
-//     return (
-//         <Modal
-//             {...props}
-//             size="lg"
-//             aria-labelledby="contained-modal-title-vcenter"
-//             centered
-//         >
-//             <Modal.Header closeButton>
-//                 <Modal.Title id="contained-modal-title-vcenter">
-//                     Modal heading
-//           </Modal.Title>
-//             </Modal.Header>
-//             <Modal.Body>
-//                 <div className="board">
-//                     <div className="board__overlay">
-//                         <h3>Assigned</h3>
-//                         <h4>Due: 01/01/01</h4>
-//                         <p>Task description goes here. Scott honks are the new greatest meme and this is just placeholder text for the lulz.</p>
-//                         <div className="profilePhotos">
-//                             <img src={Photo} alt="Photo" />
-//                             <img src={Photo} alt="Photo" />
-//                             <img src={Photo} alt="Photo" />
-//                             <img src={Photo} alt="Photo" />
-//                             <img src={Photo} alt="Photo" />
-//                             <img src={Photo} alt="Photo" />
-//                         </div>
-//                         <div className="comments">
-//                             <div className="comments">
-//                                 <img src={Photo} alt="Photo" />
-//                                 <h6>User Name</h6>
-//                                 <p>User comment about project status or issue. Whatever they need it to be.</p>
-//                             </div>
-//                         </div>
-//                         <input>
-//                         </input>
-//                         {/* <div className="board__section"> </div> */}
-//                     </div>
-//                 </div>
-//             </Modal.Body>
-//             <Modal.Footer>
-//                 <Button onClick={props.onHide}>Close</Button>
-//             </Modal.Footer>
-//         </Modal>
-//     );
-// }
-
-// function App() {
-//     const [modalShow, setModalShow] = React.useState(false);
-
-//     return (
-//         <ButtonToolbar>
-//             <Button variant="primary" onClick={() => setModalShow(true)}>
-//                 Launch vertically centered modal
-//         </Button>
-
-//             <MyVerticallyCenteredModal
-//                 show={modalShow}
-//                 onHide={() => setModalShow(false)}
-//             />
-//         </ButtonToolbar>
-//     );
-// }
-
-// render(<App />);
+export default withRouter(Card);
