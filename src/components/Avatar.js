8f36812092388d5deed99e7ac927c838c1311309
Dean Meacham <deanmeacham@rocketmail.com>
2019-07-29 11:34:18 -0400

    crab avatar test

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import crab from './../assets/avatars/crab.svg'
+import React from 'react';
+import './../sass/App.scss';
+import crab from './../assets/avatars/crab.svg';
 
 export default function Avatar() {
     return (
-        <div>
+        <div className="UserAvatars">
             <img src={crab} alt="useravatar" />
         </div>
     )
